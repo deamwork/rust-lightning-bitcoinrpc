268d6e308a2cadd5fc71bd3b5e2c3bed15de16e1
yaame <969519545@qq.com>
2019-08-13 05:32:45 +0000

    mark chaos

--- ln-manager/examples/larva_test.rs
+++ ln-manager/examples/larva_test.rs
@@ -6,6 +6,8 @@ use futures::channel::mpsc;
 use futures_timer::Interval;
 use futures::executor::{ ThreadPool, LocalPool };
 use futures::task::{ LocalSpawn };
+use futures::compat::Future01CompatExt;
+use futures01::future::Future as Future01;
 
 use std::time::{Duration};
 use std::thread;
@@ -23,10 +25,12 @@ extern crate failure;
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate tokio01;
 
 use serde::{Serialize, Deserialize};
 pub type UnboundedSender = mpsc::UnboundedSender<Pin<Box<dyn Future<Output = Result<Vec<User>, failure::Error>> + Send>>>;
-pub type Executor = tokio::runtime::TaskExecutor;
+pub type Executor = tokio01::runtime::TaskExecutor;
+pub type Executor03 = tokio::runtime::TaskExecutor;
 
 #[derive(Deserialize, Debug)]
 pub struct User {
@@ -37,6 +41,7 @@ pub struct User {
 #[derive(Clone)]
 pub struct Probe {
     exec: Executor,
+    exec03: Executor03,
 }
 
 // pub trait Larva: Clone + Sized + Send + Sync + 'static {
@@ -48,10 +53,29 @@ pub struct Probe {
 // }
 
 impl Probe {
-    pub fn new(exec: Executor) -> Self {
+    pub fn new(exec: Executor, exec03: Executor03) -> Self {
         Probe {
             exec: exec,
+            exec03,
         }
+    }
+    fn spawn_task_03(
+        &self,
+        task: impl Future<Output = Result<(), ()>> + Send + 'static,
+    ) -> Result<(), ()> {
+        // let task = Box::pin(task);
+        // let _task01 = task.map(|_| Ok::<(), ()>(())).compat();
+        self.exec03.spawn(task.map(|_| ()));
+        Ok(())
+    }
+
+    fn spawn_task_01_as_03(
+        &self,
+        task: impl Future01<Item = (), Error = ()> + Send + 'static,
+    ) -> Result<(), ()> {
+        let task03 = task.compat();
+        self.exec03.spawn(task03.map(|_| ()));
+        Ok(())
     }
 }
 
@@ -61,7 +85,9 @@ impl Larva for Probe {
         // task: impl Future<Output = Result<Vec<User>, failure::Error>> + Send + 'static,
         task: impl Future<Output = Result<(), ()>> + Send + 'static,
     ) -> Result<(), futures::task::SpawnError> {
-        self.exec.spawn(async { task.await }.map(|_| ()));
+        let task = Box::pin(task);
+        let task01 = task.map(|_| Ok::<(), ()>(())).compat();
+        self.exec.spawn(task01);
         Ok(())
     }
 }
@@ -103,10 +129,11 @@ async fn h_get_json(i: usize) -> Result<(), ()> {
     // let users = vec![ User { id: 1, name: String::from("Frank") }];
     let h_client = Arc::new(Client::new());
     let url: Uri = "http://jsonplaceholder.typicode.com/users".parse().unwrap();
-    let res = h_client.get(url).await.unwrap();
-    // // asynchronously concatenate chunks of the body
+    let res = h_client.get(url).await;
+    let res = res.unwrap();
+    // asynchronously concatenate chunks of the body
     let body = res.into_body().try_concat().await.unwrap();
-    // // try to parse as json with serde_json
+    // try to parse as json with serde_json
     let users: Vec<User> = serde_json::from_slice(&body).unwrap();
 
     println!("{}", i);
@@ -115,8 +142,8 @@ async fn h_get_json(i: usize) -> Result<(), ()> {
 }
 
 async fn local_rpc() -> Result<(), ()> {
-    // let rpc_client = Arc::new(RPCClient::new(String::from("user:pwd@10.146.15.222:18332")));
-    let rpc_client = Arc::new(RPCClient::new(String::from("admin1:123@127.0.0.1:19001")));
+    let rpc_client = Arc::new(RPCClient::new(String::from("user:pwd@10.146.15.222:18332")));
+    // let rpc_client = Arc::new(RPCClient::new(String::from("admin1:123@127.0.0.1:19001")));
     let r = rpc_client.make_rpc_call("getblockchaininfo", &[], false).await;
     println!("{:#?}", r);
     // Ok::<Vec<User>, failure::Error>(vec![User{ id: 1, name: String::from("Frank") }])
@@ -136,7 +163,7 @@ async fn run_forever() -> Result<(), failure::Error> {
     loop { }
 }
 
-fn main() -> Result<(), failure::Error> {
+fn main() {
     // runtime::raw::set_runtime(&runtime::native::Native);
     // let (rt_tx, mut rt_rx) = mpsc::unbounded::<Pin<Box<dyn Future<Output = Result<Vec<User>, failure::Error>> + Send>>>();
     // let exec = Probe::new(rt_tx);
@@ -171,21 +198,35 @@ fn main() -> Result<(), failure::Error> {
     //     }
     // }
 
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
-    let exec = Probe::new(rt.executor());
-
+    let mut rt = tokio01::runtime::Runtime::new().unwrap();
+    let mut rt3 = tokio::runtime::Runtime::new().unwrap();
+    // let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let exec = Probe::new(rt.executor(), rt3.executor());
     //test normal spawn
-    exec.clone().spawn_task( async { h_get_json(1).await } );
+    // exec.clone().spawn_task( async { h_get_json(1).await } );
 
     // test spawn in thread
     let n = exec.clone();
-    thread::spawn(move || {
-        n.clone().spawn_task( async { h_get_json(2).await } );
-        n.clone().spawn_task( async { local_rpc().await } );
-    });
+    // thread::spawn(move || {
+    //     // n.clone().spawn_task(h_get_json(2));
+    //     let task = local_rpc();
+    //     // let task = task.map(|_| Ok::<(), ()>)
+    //     n.clone().spawn_task(task);
+    // });
 
-    exec.clone().spawn_task( async { h_get_json(3).await } );
-    exec.clone().spawn_task( async { local_rpc().await } );
+    let task = h_get_json(3);
+    // let task = local_rpc();
+    // exec.clone().spawn_task( async { h_get_json(3).await } );
+    // let task = Box::pin(task);
+    // let task01 = task.map(|_| Ok::<(), ()>(())).compat();
+    // exec.clone().spawn_task_01_as_03(task01);
+    println!("++++++++++++++++++++++++++++=");
+    exec.clone().spawn_task(task);
+    // exec.clone().spawn_task_03(local_rpc());
 
-    rt.block_on(run_forever())
+    // loop { }
+    let f_task = run_forever();
+    let f_task = Box::pin(f_task);
+    // let f_task = f_task.map(|_| Ok::<(), ()>(())).compat();
+    rt3.block_on(f_task);
 }
