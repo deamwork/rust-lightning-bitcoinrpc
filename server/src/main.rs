e398fbf8f59787ee60aa85b8341bb9da45047374
yaame <969519545@qq.com>
2019-09-03 10:08:02 +0000

    remove useless

--- server/src/main.rs
+++ server/src/main.rs
@@ -55,7 +55,6 @@ fn main() {
     let node_conf = NodeSettings::new(node_conf_arg).unwrap();
 
     let log_file_name = format!("server_{}.log", &node_conf.server.address);
-    let res = TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed);
 
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap(),
